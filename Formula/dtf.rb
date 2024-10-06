585d1ba45099a480e443d62eb21cbbaa47dd6025
dv <pavan.divi@outlook.com>
2024-10-06 21:37:41 +0530

    feat: release 0.1.0

---
+++ Formula/dtf.rb
@@ -0,0 +1,13 @@
+class DtfManager < Formula
+    version "0.1.0"
+    desc "Dtf: Manage dot files"
+    homepage ""
+
+    url "https://github.com/pavandv/dtf-manager/releases/download/#{version}/dtf"
+
+    def install
+        binary_name="dtf"
+        bin.install "#{binary_name}"
+        mv bin/"#{binary_name}", bin/"dtf"
+    end
+end
